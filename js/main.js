b087e8dd8bea45d143cec27e1f4bcfe90c562007
Varga Zoltán <varga.zoltan06@gmail.com>
2020-11-30 17:52:59 +0100

    Clock

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const display = document.querySelector('.display');
+
+
+const displayClock = () => { 
+    const date = new Date()
+    console.log(date);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    display.textContent = `${hours}:${minutes}:${seconds}`
+}
+
+setInterval(displayClock, 1000);
